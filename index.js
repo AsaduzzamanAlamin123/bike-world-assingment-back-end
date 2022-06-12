d8f5e1462127d635d03b6e97cdbca5853660862b
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-06-13 04:49:56 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -27,6 +27,13 @@ async function run(){
             const result = await cursor.toArray();
             res.send(result);
 
+        });
+
+        app.get('/bike/:id' , async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bikeCollection.findOne(query);
+            res.send(result);
         })
         
 
